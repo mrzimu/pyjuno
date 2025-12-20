bab5e3d1f34bdf3e0d4962a19aa5aa777108c47e
Mingrun Li <mrli@ihep.ac.cn>
2025-12-20 19:29:03 +0800

    improve tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 import pytest
 import uproot
+import uproot_custom
+
+import pyjuno
 
 
 @pytest.fixture(scope="session")
@@ -12,3 +15,8 @@ def data_dir():
 @pytest.fixture(scope="session")
 def f_sim(data_dir):
     yield uproot.open(data_dir / "detsim.root")
+
+
+@pytest.fixture(scope="session")
+def trees_to_test():
+    yield {i.split(":")[0] for i in uproot_custom.AsCustom.target_branches}
--- tests/test_junoio.py
+++ tests/test_junoio.py
@@ -1,17 +1,37 @@
-from pathlib import Path
+import os
 
 import pytest
+import uproot
 
 import pyjuno
 
 
+def test_sim(f_sim, trees_to_test: set[str], subtests):
+    for tree_path in trees_to_test:
+        if tree_path not in f_sim:
+            continue
+        with subtests.test(tree=tree_path):
+            f_sim[tree_path].arrays()
+
+
+@pytest.mark.skipif(
+    os.getenv("PYJUNODATA") is None,
+    reason="PYJUNODATA environment variable not set",
+)
 @pytest.mark.parametrize(
-    "tree_path",
+    "fpath",
     [
-        "Event/Gen/GenHeader",
-        "Event/Sim/SimHeader",
-        "Event/Sim/SimEvt",
+        "det.root",
+        "rec.root",
+        "miniesd.root",
+        "run.root",
     ],
 )
-def test_sim(f_sim: Path, tree_path: str):
-    f_sim[tree_path].arrays()
+def test_self_hosted_files(fpath, trees_to_test: set[str], subtests):
+    f = uproot.open(os.getenv("PYJUNODATA") + "/" + fpath)
+    for tree_name in trees_to_test:
+        if tree_name not in f:
+            continue
+
+        with subtests.test(tree=tree_name):
+            f[tree_name].arrays(entry_stop=10)
